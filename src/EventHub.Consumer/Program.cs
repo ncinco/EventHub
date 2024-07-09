1d2b1e240ec71cd9ad528b12554b78cf1e8d7e17
ncinco <ncinco@gmail.com>
2024-07-10 11:05:47 +1200

    init

---
+++ src/EventHub.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+
+namespace EventHub.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeMessagesAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task ConsumeMessagesAsync()
+        {
+            var connectionString = "";
+            var eventHubName = "tests-results";
+
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            // It is recommended that you cache the Event Hubs clients for the lifetime of your
+            // application, closing or disposing when application ends.  This example disposes
+            // after the immediate scope for simplicity.
+
+            await using (var consumer = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName))
+            {
+                using var cancellationSource = new CancellationTokenSource();
+                cancellationSource.CancelAfter(TimeSpan.FromSeconds(45));
+
+                await foreach (PartitionEvent receivedEvent in consumer.ReadEventsAsync(cancellationSource.Token))
+                {
+                    // At this point, the loop will wait for events to be available in the Event Hub.  When an event
+                    // is available, the loop will iterate with the event that was received.  Because we did not
+                    // specify a maximum wait time, the loop will wait forever unless cancellation is requested using
+                    // the cancellation token.
+
+                    Console.WriteLine(receivedEvent);
+                }
+            }
+        }
+    }
+}
---
+++ src/EventHub.Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+
+namespace EventHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductMessagesAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task ProductMessagesAsync()
+        {
+            var connectionString = "";
+            var eventHubName = "tests-results";
+
+            // number of events to be sent to the event hub
+            int numOfEvents = 3;
+
+            EventHubProducerClient producerClient = new EventHubProducerClient(connectionString, eventHubName);
+
+            // Create a batch of events 
+            using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+            for (int i = 1; i <= numOfEvents; i++)
+            {
+                var eventData = new EventData(Encoding.UTF8.GetBytes($"Event {i}"));
+                eventData.Properties.Add("buildId", "1000");
+                eventData.Properties.Add("teamProjectId", "e58a3f8d-f5e0-4aca-b05c-475f664f76c0");
+
+                if (!eventBatch.TryAdd(eventData))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"Event {i} is too large for the batch and cannot be sent.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine($"A batch of {numOfEvents} events has been published.");
+                Console.ReadLine();
+            }
+            finally
+            {
+                await producerClient.DisposeAsync();
+            }
+        }
+    }
+}
